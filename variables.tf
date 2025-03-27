a6cab8ad2b6b388d739a19dbe67c864a99ec210e
RAKESH K H <Rakeshkh@outlook.in>
2025-03-27 13:18:38 +0530

    27-03-2025

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "from_port" {
+    type = number
+    default = 22
+    description = "this is the port number"
+}
+
+variable "to_port" {
+    type = number
+    default = 22
+    description = "this is the port number"
+}

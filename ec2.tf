536e821152e9b03e6ac4f28eded0091076e4cc88
RAKESH K H <Rakeshkh@outlook.in>
2025-03-27 13:40:12 +0530

    corrected the cidr blocks

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 
-resource "aws_security_group" "allow all traffic" {
+resource "aws_security_group" "allow_all_traffic" {
   name = "allow_all_traffic"
   description = "Allow all inbound and outbound traffic"
 
@@ -7,13 +7,13 @@ resource "aws_security_group" "allow all traffic" {
         from_port = var.from_port
         to_port = var.to_port
         protocol = "tcp"
-        cidr_blocks = ["0,0,0,0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     egress = {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_blocks = ["0,0,0,0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
   tags ={
     Name = "allow_all_traffic"
